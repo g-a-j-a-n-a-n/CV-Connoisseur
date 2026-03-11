2b501bb8642a4b019ad58512069d876cec534317
gajanan <realgajanandhongade@gmail.com>
2026-03-10 17:24:26 -0700

    revised review.mjs

--- netlify/functions/review.mjs
+++ netlify/functions/review.mjs
@@ -1,30 +1,55 @@
-// review.mjs
-export const handler = async (event, context) => {
-    // Only allow POST requests
-    if (event.httpMethod !== "POST") {
-        return {statusCode: 405, body: "Method Not Allowed"};
-    }
-
+export default async function review(req) {
+    console.log("Review function running");
     try {
-        //Receive the resume text from the frontend
-        const requestBody = JSON.parse(event.body);
-        const userResumeText = requestBody.resume;
 
-        // 3. Send the AI's review back to the frontend
-        // Replace 'YOUR_AI_VARIABLE' with variable that holds the AI's text message
-        return {
-            statusCode: 200,
+        const reqData = await req.json();
+        console.log(reqData);
+        const endpointURl = "https://api.groq.com/openai/v1/chat/completions";
+        const options = {
+            method: 'POST',
             headers: {
-                "Content-Type": "application/json"
+                'Authorization': `Bearer ${Netlify.env.get("API_key")}`,
+                'Content-Type': 'application/json'
             },
-            body: JSON.stringify({reply: YOUR_AI_VARIABLE})
-        };
+            body: JSON.stringify({
+                // Using Meta's free open-source Llama 3 model
+                model: "llama-3.1-8b-instant",
+                messages: [
+                    {
+                        role: "system",
+                        content: "You are an expert technical recruiter. Review the provided resume text. Give 2 strengths and 2 areas for improvement."
+                    },
+                    {
+                        role: "user",
+                        content: reqData
+                    }
+                ]
+            })
+        }
+        const response = await fetch(endpointURl, options);
+        const data = await response.json();
+
+        return new Response(
+            JSON.stringify(data),
+            {
+                status: 200,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })
 
     } catch (error) {
-        console.error("Error:", error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({error: "Failed to generate review"})
-        };
+        console.error("Error connecting to the API:", error);
+        return new Response(
+            JSON.stringify({
+                error: "Could not complete fetch request",
+            }),
+            {
+                status: 500,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            },
+            );
     }
-};
+}
